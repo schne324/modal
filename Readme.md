bb7345c9491abe937862fb2efee4c8a05f21d869
Harris Schneiderman <harris.schneiderman@deque.com>
2013-06-06 14:38:25 -0400

    first one

---
+++ Readme.md
@@ -0,0 +1,46 @@
+
+# modal
+
+  modal
+
+## Installation
+
+    $ component install schne324/modal
+
+## API
+  @param {string} ###Options:
+   	[id]: Selector for the desired modal ID (default: 'modal')
+   	[header]: Desired content for the modal header (default: 'Prompt Box!')
+   	[close]: Desired content for the close button (default: 'X')
+   	[modalContent]: Desired modal content (default: 'This is just like a prompt only it\'s not from the 70s')
+   	[inputPlaceholder]: The desired placeholder in the input (default: 'Enter a value...')
+   	[confirm]: Desired content for the submit/confirm button (default: 'Submit')
+   	[cancel]: Desired content for the cancel/dismiss button (default: 'Cancel')
+
+## Example Usage
+```javascript
+var prompter = require('modal');
+  //note: not all options must be specified, there are fall-back defualts
+  var modal = prompter('#modal-trigger', {
+    id: 'my-modal',
+    header: 'Headache!',
+    close: '(X)',
+    modalContent: 'I am the egg man, I am the catephant',
+    inputPlaceholder: 'Hold my place...',
+    confirm: 'Okay',
+    cancel: 'Nope'
+  });
+
+  modal.on('cancel', function () {
+    alert('you have clicked cancel, now all of your files are belong to us!');
+  });
+
+  modal.on('confirm', function (res) {
+    alert('you hit confirm and said ' + res);
+  });
+
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var events = require('event'),
+	query = require('query'),
+	classes = require('classes'),
+	extend = require('extend'),
+	emitter = require('emitter');
+
+
+module.exports = function (trigger, options) {
+
+	var deport = module.exports;
+
+	if (typeof trigger === 'string') {
+		trigger = query(trigger);
+	}
+
+	options = extend({
+		// these are the defaults
+		id: 'modal',
+		header: 'Prompt Box!',
+		close: 'X',
+		modalContent: 'This is just like a prompt only it\'s not from the 70s',
+		inputPlaceholder: 'Enter a value...',
+		confirm: 'Submit',
+		cancel: 'Cancel'
+	}, options);
+
+
+	var html = require('./template')
+		.replace(/\{\{id\}\}/, options.id)
+		.replace(/\{\{header\}\}/, options.header)
+		.replace(/\{\{close\}\}/, options.close)
+		.replace(/\{\{inputPlaceholder\}\}/, options.inputPlaceholder)
+		.replace(/\{\{modalContent\}\}/, options.modalContent)
+		.replace(/\{\{confirm\}\}/, options.confirm)
+		.replace(/\{\{cancel\}\}/, options.cancel);
+
+
+	var wrapper = document.createElement('div');
+	wrapper.innerHTML = html;
+	document.body.appendChild(wrapper);
+
+
+	var modal = query('#' + options.id),
+		cancel = query('.cancel', modal),
+		close = query('.close', modal),
+		confirm = query('.confirm', modal),
+		input = query('input', modal);
+
+	//fire the modal on clicks
+	events.bind(trigger, 'click', function (clickEvent) {
+		classes(modal).add('opened');
+		input.focus();
+	});
+
+
+	//close the modal
+	//clicking cancel
+	events.bind(cancel, 'click', function () {
+		classes(modal).remove('opened');
+		trigger.focus();
+
+		deport.emit('cancel');
+
+	});
+	//clicking close
+	events.bind(close, 'click', function () {
+		cancel.click();
+	});
+
+	//confirm
+	events.bind(confirm, 'click', function () {
+		if (input.value === '') {
+			input.focus();
+			return;
+		} else {
+			classes(modal).remove('opened');
+			deport.emit('confirm', input.value);
+			trigger.focus();
+		}
+
+	});
+
+
+	events.bind(modal, 'keydown', function (keyboardEvent) {
+		var which = keyboardEvent.which,
+			target = keyboardEvent.target;
+
+		//ESCAPE
+		if (which === 27) {
+			cancel.click();
+		}
+		//SHIFT + TAB
+		if (which === 9 && keyboardEvent.shiftKey) {
+			if (target === input) {
+				keyboardEvent.preventDefault();
+				close.focus();
+			} else if (target === close) {
+				keyboardEvent.preventDefault();
+				cancel.focus();
+			}
+		//TAB
+		} else if (which === 9) {
+			if (target === cancel) {
+				keyboardEvent.preventDefault();
+				close.focus();
+			}
+		//enter on input
+		} else if (which == 13) {
+			if (target === input) {
+				if (input.value === '') {
+					return;
+				} else {
+					confirm.click();
+				}
+			}
+		}
+	});
+
+	return module.exports;
+
+};
+
+
+emitter(module.exports);
