4e68698f7cf0bed78221f3794a63922e4c7c3c19
Stephen Mathieson <smath23@gmail.com>
2013-06-07 14:58:06 -0400

    refactor to support multiple modals per page
    - Added "closed", "show", and "hide" events
    - Added `Modal#hide` method
    - Added `Modal#show` method
    - Added support for multiple modals on a single page

---
+++ History.md
@@ -0,0 +1,6 @@
+# 0.1.0
+
+- Added "closed", "show", and "hide" events
+- Added `Modal#hide` method
+- Added `Modal#show` method
+- Added support for multiple modals on a single page
--- index.js
+++ index.js
@@ -4,124 +4,148 @@ var events = require('event'),
 	query = require('query'),
 	classes = require('classes'),
 	extend = require('extend'),
-	emitter = require('emitter');
+	emitter = require('emitter'),
+	template = require('./template');
 
-
-module.exports = function (trigger, options) {
-
-	var deport = module.exports;
-
-	if (typeof trigger === 'string') {
-		trigger = query(trigger);
+var Modal = module.exports = function (trigger, options) {
+	// invocation without `new` for back-compat
+	if (!(this instanceof Modal)) {
+		return new Modal(trigger, options);
 	}
 
-	options = extend({
-		// these are the defaults
-		id: 'modal',
-		header: 'Prompt Box!',
-		close: 'X',
-		modalContent: 'This is just like a prompt only it\'s not from the 70s',
-		inputPlaceholder: 'Enter a value...',
-		confirm: 'Submit',
-		cancel: 'Cancel',
-		insertInto: document.body
-	}, options);
+	var self = this;
 
+	emitter(this);
 
-	var html = require('./template')
-		.replace(/\{\{id\}\}/, options.id)
-		.replace(/\{\{header\}\}/, options.header)
-		.replace(/\{\{close\}\}/, options.close)
-		.replace(/\{\{inputPlaceholder\}\}/, options.inputPlaceholder)
-		.replace(/\{\{modalContent\}\}/, options.modalContent)
-		.replace(/\{\{confirm\}\}/, options.confirm)
-		.replace(/\{\{cancel\}\}/, options.cancel);
+	this.trigger = typeof trigger === 'string'
+		? query(trigger)
+		: trigger;
 
+	this.options = extend({}, Modal.defaults, options);
 
-	var wrapper = document.createElement('div');
-	wrapper.innerHTML = html;
-	options.insertInto.appendChild(wrapper);
+	// create our parent element
+	this.wrapper = document.createElement('div');
+	// add markup to the DOM
+	this.wrapper.innerHTML = template
+		.replace(/\{\{id\}\}/, this.options.id)
+		.replace(/\{\{header\}\}/, this.options.header)
+		.replace(/\{\{close\}\}/, this.options.close)
+		.replace(/\{\{inputPlaceholder\}\}/, this.options.inputPlaceholder)
+		.replace(/\{\{modalContent\}\}/, this.options.modalContent)
+		.replace(/\{\{confirm\}\}/, this.options.confirm)
+		.replace(/\{\{cancel\}\}/, this.options.cancel);
 
+	this.options.insertInto.appendChild(this.wrapper);
 
-	var modal = query('#' + options.id),
-		cancel = query('.cancel', modal),
-		close = query('.close', modal),
-		confirm = query('.confirm', modal),
-		input = query('input', modal);
+	this.modal = document.getElementById(this.options.id);
 
-	//fire the modal on clicks
-	events.bind(trigger, 'click', function (clickEvent) {
-		classes(modal).add('opened');
-		input.focus();
+	// opening the modal
+	events.bind(this.trigger, 'click', function () {
+		self._bind().show();
 	});
 
+	this._bind();
+};
 
-	//close the modal
-	//clicking cancel
-	events.bind(cancel, 'click', function () {
-		classes(modal).remove('opened');
-		trigger.focus();
+Modal.prototype._bind = function () {
+	var self = this,
+		cancel = this._cancel = query('.cancel', this.modal),
+		close = this._close = query('.close', this.modal),
+		confirm = this._confirm = query('.confirm', this.modal),
+		input = this._input = query('input', this.modal);
 
-		deport.emit('cancel');
-
+	// the close button
+	this._onclose = events.bind(close, 'click', function () {
+		self.emit('closed', self);
+		self.hide();
 	});
-	//clicking close
-	events.bind(close, 'click', function () {
-		cancel.click();
-	});
 
-	//confirm
-	events.bind(confirm, 'click', function () {
-		if (input.value === '') {
-			input.focus();
-			return;
-		} else {
-			classes(modal).remove('opened');
-			deport.emit('confirm', input.value);
-			trigger.focus();
+	// the confirm button
+	this._onconfirm = events.bind(confirm, 'click', function () {
+		var val = input.value;
+		if (!val) {
+			// if there's no value, just re-focus the input and return
+			return input.focus();
 		}
 
+		self.emit('confirm', val);
+		self.hide();
 	});
 
+	// cancel button
+	this._oncancel = events.bind(cancel, 'click', function () {
+		self.emit('cancel', self);
+		self.hide();
+	});
 
-	events.bind(modal, 'keydown', function (keyboardEvent) {
+	// on key press
+	this._onkeydown = events.bind(this.modal, 'keydown', function (keyboardEvent) {
 		var which = keyboardEvent.which,
 			target = keyboardEvent.target;
 
-		//ESCAPE
+		// ESCAPE
 		if (which === 27) {
-			cancel.click();
-		}
-		//SHIFT + TAB
-		if (which === 9 && keyboardEvent.shiftKey) {
-			if (target === input) {
-				keyboardEvent.preventDefault();
-				close.focus();
-			} else if (target === close) {
-				keyboardEvent.preventDefault();
-				cancel.focus();
-			}
-		//TAB
+			self._onclose();
+		// TAB
 		} else if (which === 9) {
-			if (target === cancel) {
-				keyboardEvent.preventDefault();
-				close.focus();
-			}
-		//enter on input
-		} else if (which == 13) {
-			if (target === input) {
-				if (input.value === '') {
-					return;
-				} else {
-					confirm.click();
+			// SHIFT+TAB
+			if (keyboardEvent.shiftKey) {
+				if (target === input) {
+					keyboardEvent.preventDefault();
+					close.focus();
+				} else if (target === close) {
+					keyboardEvent.preventDefault();
+					cancel.focus();
 				}
+			// TAB
+			} else {
+				if (target === cancel) {
+					keyboardEvent.preventDefault();
+					close.focus();
+				}
+			}
+		// ENTER
+		} else if (which === 13) {
+			if (target === input) {
+				self._onconfirm();
 			}
 		}
 	});
 
-	return module.exports;
-
+	return this;
 };
 
+Modal.prototype.show = function () {
+	classes(this.modal).add('opened');
+	query('input', this.modal).focus();
+	return this.emit('show', this);
+};
 
-emitter(module.exports);
+Modal.prototype.hide = function () {
+	classes(this.modal).remove('opened');
+
+console.log('trigger', this.trigger)
+
+
+	this.trigger.focus();
+
+	// remove all previous event registers
+	events.unbind(this._close, 'click', this._onclose);
+	events.unbind(this._confirm, 'click', this._onconfirm);
+	events.unbind(this._cancel, 'click', this._oncancel);
+	events.unbind(this.modal, 'keydown', this._onkeydown);
+
+	return this.emit('hide', this);
+};
+
+Modal.defaults = {
+	// these are the defaults
+	id: 'modal',
+	header: 'Prompt Box!',
+	close: 'X',
+	modalContent: 'This is just like a prompt only it\'s not from the 70s',
+	inputPlaceholder: 'Enter a value...',
+	confirm: 'Submit',
+	cancel: 'Cancel',
+	insertInto: document.body
+};
